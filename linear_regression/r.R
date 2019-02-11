73ba5b74167fe9a267fd786e6ceadd2917fe534d
Alina Grishchuk <alina.grishchuk91@gmail.com>
2019-02-12 00:02:00 +0200

    linear regression with R

---
+++ linear_regression/r.R
@@ -0,0 +1,30 @@
+library(caTools)
+library(ggplot2)
+
+dataset = read.csv('Salary_Data.csv')
+
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 0.2)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+regressor = lm(formula = Salary ~ YearsExperience,
+               data = training_set)
+y_pred = predict(regressor, newdata = test_set)
+
+ggplot() +
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (Training set)') +
+  xlab('Years of experience') +
+  ylab('Salary')
+
+ggplot() +
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (Test set)') +
+  xlab('Years of experience') +
+  ylab('Salary')
